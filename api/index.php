3a4d136e99f1275916acd4318fa1fbd96913a32f
NazmiHakim <145305124+NazmiHakim@users.noreply.github.com>
2026-03-31 20:31:56 +0800

    chore(api): enable error display to debug Vercel bootstrap

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 // Sembunyikan pesan deprecation PHP 8.5 bawaan server Vercel agar tidak merusak tampilan HTML
-error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
-ini_set('display_errors', '0');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 use Illuminate\Http\Request;
 
